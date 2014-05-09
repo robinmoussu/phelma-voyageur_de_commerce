d3ef70b1e0ab81240f3dc41cd419d3a2d86d456d
Robin Moussu <robin.moussu+github@gmail.com>
2014-05-09 08:13:45 +0200

    caractères blancs

--- fourmi.c
+++ fourmi.c
@@ -8,11 +8,11 @@
 
 Fourmi init_fourmi(Parametres *param)
 {
-	Fourmi f;
-    
+    Fourmi f;
+
     f.tabu = NULL;
-	f.L = 0;	
-	return f;
+    f.L = 0;
+    return f;
 }
 
 /** Déplace la fourmi dans la nouvelle ville, en fonction de sa visibilité et des phéromones sur l'arc
@@ -28,11 +28,11 @@ void ville_suivante(Fourmi f, Parametres *p)
     float cumul_proba;  /// < valeur permettant la normalisation des probabilités
     float fx;           /// < valeur courante de la fonction de répartition
     int i;
-    
+
     tirage = rand() / p->nb_ville;
     cumul_proba = 0;
     fx = 0;
-    
+
     for (i = 0; i < p->nb_ville; i++) {
     }
 }
@@ -49,10 +49,10 @@ int is_int_graph(Ville v, Graph g[], int size_graph)
 bool parcourt_valide(Fourmi f, Parametres p);
 
 /** Met à jour le meilleur parcourt si celui de la fourmi est meilleur
- */
+*/
 void (Fourmi f, Graph meilleur_parcourt);
 
 /** Vérifie si le parcourt de la fourmi est valide, et met à jour le graph (évaporation + nouveau phéromones)
- */
+*/
 void graph_update(Fourmi f, Graph g);
 
